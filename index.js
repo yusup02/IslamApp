d7f4d8b62ab9b53b9c8e606df67960913b4025b7
yusup02 <53394689+yusup02@users.noreply.github.com>
2022-12-19 12:03:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ $(function() {
     $('.si').slick({
         arrows: false, //скрываем кнопки
         dots: true, // добавляем точки
-        infinite: true, // бесконеное пролистывание
+        infinite: false, // бесконеное пролистывание
       slidesToShow: 1, // показываем или листаем оп одному слайду
       slidesToScroll: 1, // показываем или листаем оп одному слайду
       adaptiveHeight: true,
-      autoplay: true,
-  autoplaySpeed: 2000,
     });
     
 })
