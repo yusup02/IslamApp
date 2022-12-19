d26316875a2b883e268771ee4a5d1f22bc31ec68
yusup02 <53394689+yusup02@users.noreply.github.com>
2022-12-19 10:25:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,36 +13,3 @@ $(function() {
     });
     
 })
-
-
-
-var userDeviceArray = [
-  {device: 'Android', platform: /Android/},
-  {device: 'iPhone', platform: /iPhone/},
-  {device: 'iPad', platform: /iPad/},
-  {device: 'Symbian', platform: /Symbian/},
-  {device: 'Windows Phone', platform: /Windows Phone/},
-  {device: 'Tablet OS', platform: /Tablet OS/},
-  {device: 'Linux', platform: /Linux/},
-  {device: 'Windows', platform: /Windows NT/},
-  {device: 'Macintosh', platform: /Macintosh/}
-  ];
-
-  var platform = navigator.userAgent;
-
-  function getPlatform(yt) {
-    for (var i in userDeviceArray) {
-      if (userDeviceArray[i].platform.test(platform)) {
-        return userDeviceArray[i].device;
-      }
-    }
-    return 'Неизвестная платформа!' + platform;
-  }
-
-  if (getPlatform(yt) == 'iPhone') {
-  window.location.href = 'https://itunes.apple.com/app/id1452368807';
-  }
-
-  if (getPlatform(yt) == 'Android') {
-  window.location.href = 'http://play.google.com/store/apps/details?id=islam.islamapp';
-  }
